01786bdd8bd6f46d75f9c2e611d31a837102c6bf
E7271 <ethanvaincourt@gmail.com>
2018-10-31 09:29:23 -0700

    Update

---
+++ MovingBlock2/src/blockMover.java
@@ -0,0 +1,155 @@
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+
+public class blockMover {
+
+
+    private JPanel grid;
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("blockMover");
+        game gamePanel = new game();
+        frame.setContentPane(gamePanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.addKeyListener(gamePanel);
+        gamePanel.requestFocus();
+        while (true) {
+            //Right
+            if (gamePanel.lastKey == 1) {
+                //Makes sure you don't go into walls
+                if (gamePanel.grid[gamePanel.gridPositionY][gamePanel.gridPositionX + 1] != 0) {
+                    gamePanel.gridPositionX = gamePanel.gridPositionX + 1;
+                }
+                //Score counter, and removes pellets
+                if (gamePanel.grid[gamePanel.gridPositionY][gamePanel.gridPositionX] == 1) {
+                    gamePanel.grid[gamePanel.gridPositionY][gamePanel.gridPositionX] = 2;
+                    gamePanel.score += 1;
+                    System.out.println(gamePanel.score);
+                }
+                if (gamePanel.gridPositionX == 27){
+                    gamePanel.gridPositionX = 0;
+                }
+            }
+            //Left
+            if (gamePanel.lastKey == 2) {
+                //Makes sure you don't go into walls
+                if (gamePanel.grid[gamePanel.gridPositionY][gamePanel.gridPositionX - 1] != 0) {
+                    gamePanel.gridPositionX = gamePanel.gridPositionX - 1;
+                }
+                //Score counter, and removes pellets
+                if (gamePanel.grid[gamePanel.gridPositionY][gamePanel.gridPositionX] == 1) {
+                    gamePanel.grid[gamePanel.gridPositionY][gamePanel.gridPositionX] = 2;
+                    gamePanel.score += 1;
+                    System.out.println(gamePanel.score);
+                }
+                if (gamePanel.gridPositionX == 0){
+                    gamePanel.gridPositionX = 27;
+                }
+            }
+            //Up
+            if (gamePanel.lastKey == 3) {
+                //Makes sure you don't go into walls
+                if (gamePanel.grid[gamePanel.gridPositionY - 1][gamePanel.gridPositionX] != 0) {
+                    gamePanel.gridPositionY = gamePanel.gridPositionY - 1;
+                }
+                //Score counter, and removes pellets
+                if (gamePanel.grid[gamePanel.gridPositionY][gamePanel.gridPositionX] == 1) {
+                    gamePanel.grid[gamePanel.gridPositionY][gamePanel.gridPositionX] = 2;
+                    gamePanel.score += 1;
+                    System.out.println(gamePanel.score);
+                }
+            }
+            //Down
+            if (gamePanel.lastKey == 4) {
+                //Makes sure you don't go into walls
+                if (gamePanel.grid[gamePanel.gridPositionY + 1][gamePanel.gridPositionX] != 0) {
+                    gamePanel.gridPositionY = gamePanel.gridPositionY + 1;
+                }
+                //Score counter, and removes pellets
+                if (gamePanel.grid[gamePanel.gridPositionY][gamePanel.gridPositionX] == 1) {
+                    gamePanel.grid[gamePanel.gridPositionY][gamePanel.gridPositionX] = 2;
+                    gamePanel.score += 1;
+                    System.out.println(gamePanel.score);
+                }
+
+
+
+
+            }
+
+            Random rand = new Random();
+            int ghostDirection = rand.nextInt(4) + 1;
+            //Ghost Movement
+
+            while(true) {
+
+            //Pink Ghost
+                //right
+                if (ghostDirection == 1){
+                    //Makes sure ghost doesn't run isn't wall
+                    if (gamePanel.grid[gamePanel.pinkGhostY][gamePanel.pinkGhostX + 1] != 0) {
+                        gamePanel.pinkGhostX = gamePanel.pinkGhostX + 1;
+                    }
+                    break;
+                }
+
+
+                //left
+                if (ghostDirection == 2){
+                    //Makes sure ghost doesn't run isn't wall
+                    if (gamePanel.grid[gamePanel.pinkGhostY][gamePanel.pinkGhostX - 1] != 0) {
+                        gamePanel.pinkGhostX = gamePanel.pinkGhostX - 1;
+                    }
+                    break;
+                }
+
+
+                //up
+                if (ghostDirection == 3){
+                    //Makes sure ghost doesn't run isn't wall
+                    if (gamePanel.grid[gamePanel.pinkGhostY - 1][gamePanel.pinkGhostX] != 0) {
+                        gamePanel.pinkGhostY = gamePanel.pinkGhostY - 1;
+                    }
+                    break;
+                }
+
+
+                //down
+                if (ghostDirection == 4){
+                    //Makes sure ghost doesn't run isn't wall
+                    if (gamePanel.grid[gamePanel.pinkGhostY + 1][gamePanel.pinkGhostX] != 0) {
+                        gamePanel.pinkGhostY = gamePanel.pinkGhostY + 1;
+                        
+                    }
+                    break;
+
+                }
+                
+                break;
+            }
+
+
+
+            
+
+
+
+            // Update frame buffer
+            gamePanel.repaint();
+
+            // Add pause
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+}
+
---
+++ MovingBlock2/src/game.java
@@ -0,0 +1,216 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
+import java.util.Random;
+public class game extends JPanel implements KeyListener {
+
+    int[][] grid = new int[][]{
+            //0 is a wall piece
+            //2 is a pellet piece
+            //3 is the ghost area (No Go)
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
+            { 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
+            { 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0},
+            { 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 3, 3, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 3, 3, 3, 3, 3, 3, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 3, 3, 3, 3, 3, 3, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            { 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 3, 3, 3, 3, 3, 3, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
+            { 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
+            { 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0},
+            { 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0},
+            { 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    };
+
+    /*int[][] grid = new int[][]{
+            //0 is a wall piece
+            //2 is a pellet piece
+            //3 is the ghost area (No Go)
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    };*/
+    
+    
+    //All pixel dimensions divided by 5
+    int height = 620;
+    int width = 560;
+    int boxHeight = 20;
+    int boxWidth = 20;
+    int lastKey = 1;
+    int score = 0;
+    int gridPositionX = 20;
+    int gridPositionY = 23;
+
+
+    int pinkGhostX = 12;
+    int pinkGhostY = 14;
+
+    int cyanGhostX = 13;
+    int cyanGhostY = 14;
+
+    int orangeGhostX = 14;
+    int orangeGhostY = 14;
+
+    int redGhostX =  15;
+    int redGhostY = 14;
+
+
+
+    public game() {
+        setBackground(Color.BLACK);
+        addKeyListener(this);
+        setPreferredSize(new Dimension(width, height));
+        this.setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        Graphics2D g2player = (Graphics2D) g;
+
+
+        g2d.setColor(Color.BLUE);
+
+        // Paint walls
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[row].length; col++) {
+
+                if (grid[row][col] == 0) {
+
+                    // Pixel coordinates
+                    int x_coords = 20 * col;
+                    int y_coords = 20 * row;
+
+                    // Block dimensions
+                    int width = 20;
+                    int height = 20;
+
+                    g2d.fillRect(x_coords, y_coords, width, height);
+                }
+
+            }
+        }
+        g2d.setColor(Color.WHITE);
+        // Paint Coins
+       for (int row = 1; row < grid.length; row++) {
+            for (int col = 1; col < grid[row].length; col++) {
+
+                if (grid[row][col] == 1) {
+
+                    // Pixel coordinates
+                    int x_coords = 20 * col + 7;
+                    int y_coords = 20 * row + 7;
+
+                    // Block dimensions
+                    int width = 5;
+                    int height = 5;
+
+                    g2d.fillRect(x_coords, y_coords, width, height);
+                }
+
+            }
+        }
+
+        // Character
+        g2player.setColor(Color.YELLOW);
+        //g2player.fillRect(gridPositionX * 20, gridPositionY * 20, boxWidth, boxHeight);
+        g2player.fillRect((gridPositionX * 20) + 4, (gridPositionY * 20) + 4, 12, 12);
+
+        //Ghosts
+        Random rand = new Random();
+        int ghostMovement = rand.nextInt(4) + 1;
+
+        g2player.setColor(Color.PINK);
+        g2player.fillRect(pinkGhostX * 20, pinkGhostY * 20, boxWidth, boxHeight);
+        g2player.setColor(Color.CYAN);
+        g2player.fillRect(cyanGhostX * 20, cyanGhostY * 20, boxWidth, boxHeight);
+        g2player.setColor(Color.ORANGE);
+        g2player.fillRect(orangeGhostX * 20, orangeGhostY * 20, boxWidth, boxHeight);
+        g2player.setColor(Color.RED);
+        g2player.fillRect(redGhostX * 20, redGhostY * 20, boxWidth, boxHeight);
+    }
+
+
+    @Override
+    public void keyTyped(KeyEvent e) { }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch(e.getKeyCode()) {
+            //Right
+            case KeyEvent.VK_RIGHT:
+                lastKey = 1;
+                break;
+
+            //Left
+            case KeyEvent.VK_LEFT:
+                lastKey = 2;
+                break;
+
+            //Up
+            case KeyEvent.VK_UP:
+                lastKey = 3;
+                break;
+
+            case KeyEvent.VK_DOWN:
+                lastKey = 4;
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) { }
+}
