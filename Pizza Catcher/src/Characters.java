4859d60103cf3752475ef44f97bd99eebf5394c4
E7271 <ethanvaincourt@gmail.com>
2018-12-01 19:02:47 -0800

    Working on Pizza Catcher

--- Pizza Catcher/src/Characters.java
+++ Pizza Catcher/src/Characters.java
@@ -1,9 +1,10 @@
 public class Characters {
-    public int playerPositionX = 1 ;
+
+    public int playerPositionX = 25 ;
     public int playerPositionY = 1 ;
 
-    public int playerWidth = 75;
-    public int playerHeight = 75;
+    public int playerWidth = 21;
+    public int playerHeight = 21;
 
 
 
