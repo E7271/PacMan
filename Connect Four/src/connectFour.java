b8743dbd1c1eb5776097a0e6fd5885c93cd30280
E7271 <ethanvaincourt@gmail.com>
2018-12-31 23:12:51 -0800

    Update connectFour.java

--- Connect Four/src/connectFour.java
+++ Connect Four/src/connectFour.java
@@ -1,9 +1,9 @@
 import javax.swing.*;
 import java.util.Scanner;
-
 public class connectFour {
     public static gameboard gb = new gameboard();
-
+    public static input input = new input();
+    public static int playerTurn = 1;
     public static void main(String[] args) {
         // Create the window to hold the game
         JFrame window = new JFrame("Game");
@@ -16,46 +16,57 @@ public class connectFour {
         window.pack();
         window.setVisible(true);
         while (true) {
+            if (playerTurn == 3){
+                playerTurn = 2;
+            }
+            if (playerTurn == 4){
+                playerTurn = 1;
+            }
+            Scanner in = new Scanner(System.in);
+            System.out.println("Which column would you like to drop into? ");
+            int playerChoice = in.nextInt();
             /*
             This Section Deals with Player One Input
              */
-            Scanner in = new Scanner(System.in);
-            System.out.println("Which column would you like to drop into? ");
-            int playerOneChoice = in.nextInt();
-
-
-            int valueForPlayers = 2;
-
-            int playerTurn = 1;
-
             if (playerTurn == 1) {
-                for (int row = (playerOneChoice - 1); row <= 5; row += 1) {
-                    if (gb.connectFourBoard[row][(playerOneChoice - 1)] == 1 || gb.connectFourBoard[row][(playerOneChoice - 1)] == 2) {
-                        gb.connectFourBoard[row - 1][(playerOneChoice - 1)] = 1;
-                        System.out.println("Full Space");
-                        playerTurn = 2;
+                for (int row = (playerChoice - 1); row <= 6; row += 1) {
+                    if (gb.connectFourBoard[5][playerChoice - 1] == 0) {
+                        gb.connectFourBoard[5][playerChoice - 1] = 1;
                         break;
                     }
-                    if (gb.connectFourBoard[row][(playerOneChoice - 1)] == 0) {
+                    if (gb.connectFourBoard[row][(playerChoice - 1)] == 1 || gb.connectFourBoard[row][(playerChoice - 1)] == 2) {
+                        gb.connectFourBoard[row - 1][(playerChoice - 1)] = 1;
+                        System.out.println("Full Space");
+                        break;
+                    }
+                    if (gb.connectFourBoard[row][(playerChoice - 1)] == 0) {
                         System.out.println("Blank Space");
                     }
                 }
+                playerTurn = 3;
             }
+            /*
+            This Section Deals with Player Two Input
+             */
             if (playerTurn == 2) {
-                for (int row = (playerOneChoice - 1); row <= 5; row += 1) {
-                    if (gb.connectFourBoard[row][(playerOneChoice - 1)] == 1 || gb.connectFourBoard[row][(playerOneChoice - 1)] == 2) {
-                        gb.connectFourBoard[row - 1][(playerOneChoice - 1)] = 2;
-                        System.out.println("Full Space");
-                        playerTurn = 1;
+                for (int row = (playerChoice - 1); row <= 6; row += 1) {
+
+                    System.out.println("For Loop Runs ");
+                    if (gb.connectFourBoard[5][playerChoice - 1] == 0) {
+                        gb.connectFourBoard[5][playerChoice - 1] = 2;
                         break;
                     }
-                    if (gb.connectFourBoard[row][(playerOneChoice - 1)] == 0) {
+                    if (gb.connectFourBoard[row][(playerChoice - 1)] == 1 || gb.connectFourBoard[row][(playerChoice - 1)] == 2) {
+                        gb.connectFourBoard[row - 1][(playerChoice - 1)] = 2;
+                        System.out.println("Full Space");
+                        break;
+                    }
+                    if (gb.connectFourBoard[row][(playerChoice - 1)] == 0) {
                         System.out.println("Blank Space");
                     }
                 }
+                playerTurn = 4;
             }
-
-
             gameboard.repaint();
             // Add pause
             try {
@@ -63,7 +74,6 @@ public class connectFour {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
             System.out.println(gb.connectFourBoard[0][0]);
             System.out.println(gb.connectFourBoard[1][0]);
             System.out.println(gb.connectFourBoard[2][0]);
@@ -71,11 +81,8 @@ public class connectFour {
             System.out.println(gb.connectFourBoard[4][0]);
             System.out.println(gb.connectFourBoard[5][0]);
 
+
+
         }
-
     }
-
-
 }
-
-
